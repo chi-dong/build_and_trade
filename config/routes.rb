9e2c082a433b4d5d7a0f18d26ef6dca35f3fd0bb
Chi-Dong Ly <lychhh@googlemail.com>
2020-03-23 01:16:07 +0100

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root to: 'welcome#index'
+
+  devise_for :users
   resources :games
+
   get 'welcome', to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
