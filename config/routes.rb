24f70c197d36b5d6821287fbe25557841f85ed04
Chi-Dong Ly <lychhh@googlemail.com>
2020-03-23 02:15:45 +0100

    contoller auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'welcome#index'
-
   devise_for :users
   resources :games
+
+  root to: 'welcome#index'
 
   get 'welcome', to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
